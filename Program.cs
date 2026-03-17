21646047e6414075288fcd64144f6fa95deb0c92
Néstor Fabio Muñoz García <nmunoz@ttiventures.com>
2026-03-17 10:35:25 +0100

    Fixed a bug where the last date was not assigned correctly.

--- Program.cs
+++ Program.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections.ObjectModel;
 
-DateOnly start = new DateOnly(2026, 3, 11);
-int hours = 489;
+DateOnly start = new DateOnly(2026, 3, 18);
+int hours = 400;
 double hoursByDay = 8;
 Collection<DateOnly> holidays = [
-    new DateOnly(2026, 3, 20),
+    //new DateOnly(2026, 3, 20),
+    new DateOnly(2026, 3, 30),
+    new DateOnly(2026, 3, 31),
+    new DateOnly(2026, 4, 1),
     new DateOnly(2026, 4, 2),
     new DateOnly(2026, 4, 3),
     new DateOnly(2026, 4, 6),
+    new DateOnly(2026, 4, 7),
     new DateOnly(2026, 5, 1),
+    new DateOnly(2026, 5, 4),
+    new DateOnly(2026, 6, 8),
     new DateOnly(2026, 6, 9)
 ];
 
@@ -20,14 +26,13 @@ Console.WriteLine("Días " + days);
 DateOnly end = start;
 while (days > 0)
 {
-    
     if (NeitherWeekendNorHolidays(end, holidays))
     {
         days--;
     }
     end = end.AddDays(1);
 }
-end.AddDays(-1);
+end = end.AddDays(-1);
 
 Console.WriteLine(end);
 
