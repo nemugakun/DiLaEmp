854bd127b773ac6b501d0c491638cd79226646d6
Néstor Fabio Muñoz García <nemugakun@gmail.com>
2026-03-19 13:13:39 +0100

    Calendar added without using its own data to the algorithm.

--- src/DiLaEmp/Program.cs
+++ src/DiLaEmp/Program.cs
@@ -1,6 +1,7 @@
 using DiLaEmp;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
 using System.Globalization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -9,5 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddLocalization();
+builder.Services.AddRadzenComponents();
 
 await builder.Build().RunAsync();
