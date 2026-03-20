3d197ab04794ec46f64809741cd8a7bd02765a3d
Néstor Fabio Muñoz García <nemugakun@gmail.com>
2026-03-20 12:47:49 +0100

    Now the Calendar feature is fiinshed.

---
+++ src/DiLaEmpLibrary/Objects/Calendar.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiLaEmpLibrary.Objects
+{
+    public class Calendar
+    {
+        public SortedSet<DateOnly>? Dates {  get; set; }
+        public string? Name { get; set; }
+    }
+}
--- src/DiLaEmpLibrary/Utils/DateUtils.cs
+++ src/DiLaEmpLibrary/Utils/DateUtils.cs
@@ -15,5 +15,28 @@ namespace DiLaEmpLibrary.Utils
         {
             return dt.DayOfWeek;
         }
+
+        public static SortedSet<DateOnly> GetDateOnlysFromDateTimes(ICollection<DateTime?> dates)
+        {
+            SortedSet<DateOnly> dateOnlyHolidays = new SortedSet<DateOnly>();
+            foreach (var date in dates)
+            {
+                if (date is not null)
+                {
+                    dateOnlyHolidays.Add(DateOnly.FromDateTime(date.Value));
+                }
+            }
+            return dateOnlyHolidays;
+        }
+
+        public static SortedSet<DateTime?> GetDateTimesFromDateOnlys(SortedSet<DateOnly> dates)
+        {
+            SortedSet<DateTime?> dateTimes = new SortedSet<DateTime?>();
+            foreach (var date in dates)
+            {
+                dateTimes.Add(date.ToDateTime(TimeOnly.MinValue));
+            }
+            return dateTimes;
+        }
     }
 }
